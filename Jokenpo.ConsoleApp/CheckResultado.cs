2485eecb3bae624d39080d4ec82ae003b641f94c
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-03-30 19:11:00 -0300

    adicionado o README no projeto

--- Jokenpo.ConsoleApp/CheckResultado.cs
+++ Jokenpo.ConsoleApp/CheckResultado.cs
@@ -8,9 +8,14 @@ namespace Jokenpo.ConsoleApp
             if (decisaoDoJogador == escolhaComputador)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("------------------------------------------------------------------------------------");
+                Console.WriteLine($"Sua escolha foi {decisaoDoJogador} a escolha do computador foi {escolhaComputador}");
+                Console.WriteLine("------------------------------------------------------------------------------------");
+
                 Console.WriteLine("O jogo foi empatado entao será resetado");
                 Console.WriteLine("Precione ENTER para proceguir...");
                 Console.ReadLine();
+
                 Console.ResetColor();
             }
             else if (decisaoDoJogador == "Papel" && escolhaComputador == "Pedra" ||
@@ -19,14 +24,20 @@ namespace Jokenpo.ConsoleApp
 
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("");
+                Console.WriteLine("------------------------------------------------------------------------------------");
+                Console.WriteLine($"Sua escolha foi {decisaoDoJogador} a escolha do computador foi {escolhaComputador}");
+                Console.WriteLine("------------------------------------------------------------------------------------");
+
                 Console.WriteLine("Você venceu");
                 Console.ResetColor();
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("");
+                Console.WriteLine("------------------------------------------------------------------------------------");
+                Console.WriteLine($"Sua escolha foi {decisaoDoJogador} a escolha do computador foi {escolhaComputador}");
+                Console.WriteLine("------------------------------------------------------------------------------------");
+
                 Console.WriteLine("O computador venceu");
                 Console.ResetColor();
             }
---
+++ README.md
@@ -0,0 +1,62 @@
+# Jogo Pedra, Papel e Tesoura
+
+![](docs\JokenpoVideo.gif)
+
+## Descrição
+
+Este projeto consiste em um jogo digital de Pedra, Papel e Tesoura desenvolvido em **C# utilizando aplicação de console.**
+O jogador humano compete contra o computador, que realiza jogadas de forma aleatória.
+
+O objetivo do jogo é vencer as rodadas seguindo as regras clássicas:
+
+- Pedra vence Tesoura
+- Tesoura vence Papel
+- Papel vence Pedra
+
+## Objetivo do Projeto
+
+Este projeto foi desenvolvido como exercício de lógica de programação no curso Academia do Programador, com o objetivo de praticar:
+
+- Entrada e saída de dados no console
+- Estruturas de decisão (if, switch)
+- Estruturas de repetição (while, for)
+- Geração de números aleatórios
+- Organização de código em métodos
+
+## Funcionalidades (MVP)
+### Iniciar o jogo
+
+O sistema permite iniciar uma nova partida onde o jogador pode escolher entre:
+
+- Pedra
+- Papel
+- Tesoura
+
+### Jogada do computador
+
+O computador gera automaticamente uma jogada aleatória.
+
+### Determinar vencedor
+
+O sistema compara as escolhas e determina o resultado da rodada:
+
+- Empate (jogadas iguais)
+- Vitória do jogador
+- Vitória do computador
+
+### Exibir resultado
+
+Ao final de cada rodada, o sistema exibe:
+
+- Jogada do jogador
+- Jogada do computador
+- Resultado da rodada
+
+### Múltiplas rodadas
+
+O jogador pode continuar jogando várias rodadas sem precisar reiniciar o programa.
+
+### Tecnologias utilizadas
+- C#
+- .NET
+- Console Application
