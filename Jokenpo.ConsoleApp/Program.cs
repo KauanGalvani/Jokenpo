387f5d9032a512acc6c64fb1356de82aaab24052
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-03-30 17:00:05 -0300

    adicionado o codigo principal

--- Jokenpo.ConsoleApp/Program.cs
+++ Jokenpo.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Jokenpo.ConsoleApp
+﻿using System.Security.Cryptography;
+
+namespace Jokenpo.ConsoleApp
 {
     class Program
     {
@@ -24,18 +26,56 @@
 
                 switch (escolhaJogador)
                 {
-                    case "1":
-                        escolhaJogador = "Papel";
-                        break;
-                    case "2":
-                        escolhaJogador = "Pedra";
-                        break;
-                    case "3":
-                        escolhaJogador = "Tesoura";
-                        break;
+                    case "1": escolhaJogador = "Papel"; break;
+                    case "2": escolhaJogador = "Pedra"; break;
+                    case "3": escolhaJogador = "Tesoura"; break;
                     default:
                         Console.WriteLine("escolha uma opção valida. ");
+                        Console.ReadLine();
+                        Console.Clear();
                         continue;
+                }
+
+                string[] jokenpoPC = ["Pedra", "Papel", "Tesoura"];
+
+                int indiceAleaorio = RandomNumberGenerator.GetInt32(jokenpoPC.Length); //aqui gera a escolha do Computador.
+                string escolhaPC = jokenpoPC[indiceAleaorio];
+
+                if (escolhaJogador == escolhaPC)
+                {
+                    Console.WriteLine("O jogo foi empatado entao será resetado");
+                    Console.WriteLine("Precione ENTER para proceguir...");
+                    Console.ReadLine();
+                    continue;
+                }
+                else if (escolhaJogador == "Papel" && escolhaPC == "Pedra" ||
+                        escolhaJogador == "Pedra" && escolhaPC == "Tesoura" ||
+                        escolhaJogador == "Tesoura" && escolhaPC == "Papel")
+
+                {
+                    Console.WriteLine("Você venceu");
+                }
+                else
+                {
+                    Console.WriteLine("O computador venceu");
+                }
+
+                Console.WriteLine("");
+                
+                Console.WriteLine("Deseja jogar novamente? (S/N)");
+                string? devContinuar = Console.ReadLine()?.ToUpper();
+
+                if (devContinuar != "S")
+                {
+                    Console.WriteLine("Você esta fechando o jogo obrigado por jogar...");
+                    Console.WriteLine("Pressione ENTER para sair...");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    continue;
                 }
 
             }
